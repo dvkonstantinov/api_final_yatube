14a01cacbc9349756798bed55551f616e2cff839
dvkonstantinov <121demon@mail.ru>
2022-04-09 17:40:22 +0300

    v1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
+    # group = SlugRelatedField(slug_field='slug',
+    #                          queryset=Group.objects.all(), required=False)
+    comments = PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         fields = '__all__'
@@ -17,7 +21,33 @@ class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
+    text = serializers.CharField()
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(slug_field='username', read_only=True)
+    following = SlugRelatedField(slug_field='username',
+                                 queryset=User.objects.all())
+
+    def validate_following(self, follower_name):
+        user = self.context.get('request').user
+        if user == follower_name:
+            raise serializers.ValidationError('Вы не можете подписаться на '
+                                              'самого себя')
+        return follower_name
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,62 @@
-# TODO:  Напишите свой вариант
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, filters
+from rest_framework.exceptions import ValidationError
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import (PostSerializer, CommentSerializer,
+                          GroupSerializer, FollowSerializer)
+from posts.models import *
+from .permissions import IsAuthorOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    def get_queryset(self):
+        pk = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=pk)
+        return Comment.objects.filter(post=post)
+
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=pk)
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        try:
+            serializer.save(user=user)
+        except IntegrityError:
+            raise ValidationError(
+                'Вы уже подписаны на этого человека')
+
+    def get_queryset(self):
+        user = self.request.user
+        return Follow.objects.filter(user=user)
